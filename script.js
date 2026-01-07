ea7b6410da51300f5d7af0e4ed484280f27618d8
IANULA CONDREA <andreww.steel69@gmail.com>
2026-01-07 11:10:27 +0200

    create book card forEach book in array

--- script.js
+++ script.js
@@ -45,3 +45,45 @@ function toogleBookRead(id) {
     }
 }
 
+function displayBooks() {
+    libraryContainer.innerHTML = "";
+
+    if(myLibrary.length === 0) {
+        libraryContainer.appendChild(emptyState);
+        emptyState.style.display = "flex";
+        return;
+    }
+
+    myLibrary.forEach(book => {
+        const card = document.createElement("div");
+        card.classList.add("book-card");
+
+        card.dataset.id = book.id;
+
+        const titleEl = document.createElement("h3");
+        titleEl.textContent = book.title;
+
+        const authorEl = document.createElement("div");
+        authorEl.classList.add("book-info");
+
+        const authorIcon = document.createElement("span");
+        authorIcon.className = "mdi mdi-account-outline";
+
+        const authorText = document.createElement("span");
+        authorText.textContent = book.author;
+
+        authorEl.append(authorIcon, authorText);
+
+        const pagesEl = document.createElement("div");
+        pagesEl.classList.add("book-info");
+
+        const pagesIcon = document.createElement("span");
+        pagesIcon.className = "mdi mdi-book-open-page-variant-outline";
+        
+        const pagesText = document.createElement("span");
+        pages.textContent = `${book.pages} pages`;
+
+        pagesEl.append(pagesIcon, pagesText);
+        
+    })
+}

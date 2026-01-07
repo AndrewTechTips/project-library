990c9f37713f009692e0fe8c17265afc7cdac18a
IANULA CONDREA <andreww.steel69@gmail.com>
2026-01-07 10:55:47 +0200

    add remove and add functions for books , and read status

--- script.js
+++ script.js
@@ -19,3 +19,29 @@ const bookDialog = document.getElementById("bookDialog");
 const bookForm = document.getElementById("bookForm");
 const addBookBtn = document.getElementById("addBookBtn");
 const closeDialogBtn = document.getElementById("closeDialogBtn");
+
+function addBookToLibrary(title, author, pages, isRead) {
+    const newBook = new Book(title, author, pages, isRead);
+    myLibrary.push(newBook);
+
+    displayBooks();
+}
+
+function removeBook(id) {
+    const index = myLibrary.findIndex(book => book.id === id);
+
+    if(index > -1) {
+        myLibrary.splice(index, 1);
+        displayBooks();
+    }
+}
+
+function toogleBookRead(id) {
+    const book = myLibrary.find(book => book.id === id);
+
+    if(book) {
+        book.toggleReadStatus();
+        displayBooks();
+    }
+}
+

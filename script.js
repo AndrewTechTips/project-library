cd7c88a123e6f903c59e0cd70c42a71316326176
IANULA CONDREA <andreww.steel69@gmail.com>
2026-01-07 10:46:42 +0200

    add constructor for book and retrieve doc elements

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const addBookBtn = document.getElementById('addBookBtn');
-const bookDialog = document.getElementById('bookDialog');
-const closeDialogBtn = document.getElementById('closeDialogBtn');
+const myLibrary = [];
 
-addBookBtn.addEventListener('click', () => bookDialog.showModal());
-closeDialogBtn.addEventListener('click', () => bookDialog.close());
+function Book(title, author, pages, isRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+Book.prototype.toggleReadStatus = function() {
+    this.isRead = !this.isRead;
+};
+
+const libraryContainer = document.getElementById("libraryContainer");
+const emptyState = document.getElementById("emtpyState");
+
+const bookDialog = document.getElementById("bookDialog");
+const bookForm = document.getElementById("bookForm");
+const addBookBtn = document.getElementById("addBookBtn");
+const closeDialogBtn = document.getElementById("closeDialogBtn");
